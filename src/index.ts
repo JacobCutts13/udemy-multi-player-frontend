73999e2cded74304a1aae837446d25d1b9707e9a
unknown <jacobdcutts@gmail.com>
2022-05-22 14:01:21 +0100

    added rooms/index.html with messaging form

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,48 @@
-import {
-    Scene,
-    Mesh,
-    MeshStandardMaterial,
-    BoxBufferGeometry,
-} from 'three';
-import { setupCamera } from './setupCamera';
-import { setupHelpers } from './setupHelpers';
-import { setupLights } from './setupLights';
-import { setupOrbitControls } from './setupOrbitControls';
-import { setupRenderer } from './setupRenderer';
+import { Scene, Mesh, MeshStandardMaterial, BoxBufferGeometry } from "three";
+import { setupCamera } from "./setupCamera";
+import { setupHelpers } from "./setupHelpers";
+import { setupLights } from "./setupLights";
+import { setupOrbitControls } from "./setupOrbitControls";
+import { setupRenderer } from "./setupRenderer";
 
 export function setupThreeJSScene() {
+  let dimensions = { w: window.innerWidth, h: window.innerHeight };
 
-    let dimensions = { w: window.innerWidth, h: window.innerHeight };
+  const camera = setupCamera(dimensions);
 
-    const camera = setupCamera(dimensions);
+  const renderer = setupRenderer(camera, dimensions);
 
-    const renderer = setupRenderer(camera, dimensions);
+  const controls = setupOrbitControls(camera, renderer.domElement);
 
-    const controls = setupOrbitControls(camera, renderer.domElement);
+  let scene = new Scene();
 
-    let scene = new Scene();
+  setupLights(scene);
 
-    setupLights(scene);
+  setupHelpers(scene);
 
-    setupHelpers(scene);
+  //shape(s)
+  const geometry = new BoxBufferGeometry(10, 10, 10);
+  const material = new MeshStandardMaterial({
+    color: 0xff00ff,
+  });
 
-    //shape(s)
-    const geometry = new BoxBufferGeometry(10, 10, 10);
-    const material = new MeshStandardMaterial({
-        color: 0xff00ff
-    });
+  let myShape: Mesh = new Mesh(geometry, material);
+  myShape.position.y = 20;
+  scene.add(myShape);
 
-    let myShape: Mesh = new Mesh(geometry, material);
-    myShape.position.y = 20;
-    scene.add(myShape);
+  animate();
 
+  function animate() {
+    myShape.rotation.y += 0.01;
+    myShape.rotation.x += 0.02;
 
-    animate();
+    renderer.render(scene, camera);
 
+    // required if controls.enableDamping or controls.autoRotate are set to true
+    controls.update();
 
-    function animate() {
-        myShape.rotation.y += 0.01;
-        myShape.rotation.x += 0.02;
-
-        renderer.render(scene, camera);
-
-        // required if controls.enableDamping or controls.autoRotate are set to true
-        controls.update();
-
-        requestAnimationFrame(animate);
-    }
+    requestAnimationFrame(animate);
+  }
 }
 
 setupThreeJSScene();
